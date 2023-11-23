b0fa5e5b15b67355924e6123230c7a319cb187f6
AlexnderSoto <142024815+AlexnderSoto@users.noreply.github.com>
2023-11-23 09:31:54 -0600

    Update Historia.js

--- Proyecto LIC/js/Historia.js
+++ Proyecto LIC/js/Historia.js
@@ -1,12 +1,12 @@
 
-const userRole = localStorage.getItem('userRole');
+const userType = localStorage.getItem('userType');
 const agregarGestorMenuItem = document.querySelector('.agregar-gestor');
 
 // Verifica el rol del usuario y muestra u oculta contenido según sea necesario
 const formularioConciertos = document.getElementById('formulario-conciertos');
 
 // Verifica el rol y muestra u oculta el formulario
-if (userRole === 'admin') {
+if (userType === 'admin') {
     agregarGestorMenuItem.style.display = 'block';
 } else {
     agregarGestorMenuItem.style.display = 'none';
