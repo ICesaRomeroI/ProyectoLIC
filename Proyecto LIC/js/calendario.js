cb30522b123fb89f51182aac7d29c36124c88676
JonathanRodriguezC <89150664+JonathanRodriguezC@users.noreply.github.com>
2023-11-23 07:48:51 -0600

    Update calendario.js
    implementacion de los eventos en js para poder almacenar los proximos eventos en firebase y mostrarlos en html

--- Proyecto LIC/js/calendario.js
+++ Proyecto LIC/js/calendario.js
@@ -1,12 +1,12 @@
 import {
-  saveVenta
+  saveVenta, saveEvent, getEventListSize, getEvento, getEventos
 } from "./firebase.js";
 
 const userType = localStorage.getItem('userType');
 const agregarGestorMenuItem = document.querySelector('.agregar-gestor');
 
 // Verifica el rol del usuario y muestra u oculta contenido según sea necesario
-const formularioConciertos = document.getElementById('formulario-conciertos');
+const formularioConciertos = document.querySelector('.ConcertAdm');
 const formularioConciertosusuario = document.getElementById('formulario-conciertos-usuarios');
 
 // Verifica el rol y muestra u oculta el formulario
@@ -112,3 +112,145 @@ async function AddData() {
     console.error('Error al procesar los datos:', error);
   }
 }
+
+//Agregar Eventos
+
+
+
+async function AddEvent() {
+  if (validateData()) {
+    // Leyendo los valores de los campos
+    let name = document.getElementById("name").value;
+    let description = document.getElementById("description").value;
+    let image = document.getElementById("inputGroupFile01");
+    let reader = new FileReader();
+    reader.readAsDataURL(image.files[0]);
+    reader.addEventListener("load", () => {
+      // Guardamos el registro en firebase
+      saveEvent({
+        name, description, image: reader.result
+      });
+    });
+
+    // Limpiando campos
+    document.getElementById("name").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("inputGroupFile01").value = "";
+    // Cerrando el modal
+    document.getElementById("close-btn").click();
+    alertify.success('Product added succesfully');
+    showData();
+  }
+}
+//mostrar Datos
+async function showData() {
+
+  let html = "";
+  // Recuperando el tamaño de la lista de productos
+  let size = await getEventListSize();
+
+  if (size == 0) { // Si no hay productos en la colección
+    html = `<div class="card-body">
+              <div class="row gx-2">
+              <div class="col">
+              <div class="p-3">
+                  <img src="img/no-data-found.png" class="img-fluid d-block">
+              </div></div></div></div>`;
+
+  }
+  else { // Si hay productos
+    //Recuperando los productos
+    const productList = await getEventos();
+    productList.forEach(element => {
+      console.log(element);
+      const product = element.data();
+      html += `
+
+                      <div >
+                      
+                      <div class="event col-md-5">
+                      <h2 style="color:red">Proximamente</h2>
+                          <img src="${product.image}" alt="" class="Img_noticia">
+                          <div class="date">
+                              <p>Fecha: ${product.name}</p>
+                          </div>
+                          <div class="description">
+                              <p><b>Lugar:</b></p>
+                              <p id="lugar">${product.description}</p>
+                          </div>
+                      </div>
+                      </div>
+                                              
+          `;
+    });
+  }
+  document.getElementById("crud-table").innerHTML = html;
+}
+
+showData();
+
+
+
+
+let addButton = document.getElementById("submitdata");
+// Agrega un listener para el evento de click
+addButton.addEventListener("click", AddEvent);
+
+//validar los datos del modal
+function validateData() {
+  let name = document.getElementById("name").value.trim();
+  let description = document.getElementById("description").value.trim();
+  let image = document.getElementById("inputGroupFile01");
+  if (name == "") {
+    document.getElementById("name-error-msg").innerHTML = "You must enter the name";
+    return false;
+  }
+  else {
+    document.getElementById("name-error-msg").innerHTML = "";
+  }
+
+  if (description == "") {
+    document.getElementById("disc-error-msg").innerHTML = "You must enter the description";
+    return false;
+  }
+  else if (description.length > 100) {
+    document.getElementById("disc-error-msg").innerHTML = "Description max length is 100";
+    return false;
+  }
+  else {
+    document.getElementById("disc-error-msg").innerHTML = "";
+  }
+
+  if (image.files.length == 0) {
+    document.getElementById("image-error-msg").innerHTML = "You must select a image";
+    return false;
+  }
+  else {
+    document.getElementById("image-error-msg").innerHTML = "";
+  }
+
+  let allowedFormats = /(\.jpg|\.jpeg|\.png|\.webp)$/i;
+  if (!allowedFormats.exec(image.files[0].name)) {
+    document.getElementById("image-error-msg").innerHTML = "You must select a valid image";
+    image.value = "";
+    return false;
+  }
+  else {
+    document.getElementById("image-error-msg").innerHTML = "";
+  }
+
+  let fileSize = image.files[0].size / 1024;
+  if (fileSize > 700) {
+    document.getElementById("image-error-msg").innerHTML = "You max file size is 700 KB";
+    image.value = "";
+    return false;
+  }
+  else {
+    document.getElementById("image-error-msg").innerHTML = "";
+  }
+
+
+
+  return true;
+}
+
